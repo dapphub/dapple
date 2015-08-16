2207a6ba91c87ae50d7a79a595d35c2fbf313b2c
Nikolai Mushegian <nikolai.mushegian@gmail.com>
2015-08-16 12:24:23 -0400

    idk

--- test.py
+++ test.py
@@ -2,15 +2,19 @@ from ethertdd import EvmContract
 import json
 import yaml
 import sys
+import re
 
 abi, binary = None, None
 suite = {}
-def run_tests(pack):
+def run_tests(pack, testregex=None):
     for typename, info in pack.iteritems():
-        if typename == "Test":
+        if testregex is not None:
+            if not re.match(".*"+testregex+".*", typename, flags=re.IGNORECASE):
+                continue
+        if typename == "Test": # base test matches too often
             continue
-        if info["binary"] == "":
-            #print "Skipping abstract class", typename
+        
+        if info["binary"] == "": # Abstract classes
             continue
         abi = info["json-abi"]
         jabi = json.loads(abi)
@@ -24,14 +28,14 @@ def run_tests(pack):
         print "Testing", typename
         binary = info["binary"].decode('hex')
         try:
-            tmp = EvmContract(abi, binary, typename, [], gas=10**10) 
+            tmp = EvmContract(abi, binary, typename, [], gas=10**9) 
         except Exception, e:
         #    print typename, info
             raise e
         for func in dir(tmp):
             if func.startswith("test"):
                 print "  " + func
-                contract = EvmContract(abi, binary, typename, [], gas=10**10) 
+                contract = EvmContract(abi, binary, typename, [], gas=10**9) 
                 if hasattr(contract, "setUp"):
                     contract.setUp()
                 getattr(contract, func)()
