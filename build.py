844ec0fbc6e7c70be54fc153d29fb361e54151b6
Nikolai Mushegian <nikolai.mushegian@gmail.com>
2015-08-19 22:09:49 -0400

    support exporting binaries in modules

--- build.py
+++ build.py
@@ -4,14 +4,19 @@ import json
 
 # Module name to list of paths
 modules = {}
+bins = []
 
-def get_paths(path, build=None):
+def get_paths(path, srcs=None):
     if not path.endswith("/"):
         path += "/"
-    if build == None:
+    if srcs == None:
         build = yaml.load(open(path + "/module.yaml"))
+        if "bins" in build.keys():
+            for k, v in build["bins"].iteritems():
+                bins.append(k)
+        srcs = build["srcs"]
     paths = []
-    for k, v in build.iteritems():
+    for k, v in srcs.iteritems():
         if v == True:
             paths.append(path + k)
         elif v != False: # not a bool
@@ -32,12 +37,19 @@ def compile_sources(paths, cwd):
     return pack
 
 
+
 def load_module(module_name, app_yaml):
     module = app_yaml[module_name]
-    if "keps" in module.keys():
+    if "deps" in module.keys():
         for dep in module["deps"]:
             if dep not in modules.keys():
                 load_module(dep, app_yaml)
     paths = get_paths(module["src"])
     modules[module_name] = paths
 
+def write_binaries(path, pack, names):
+    bins = []
+    with open(path, "w") as f:
+        for name in names:
+            bins.append(pack[name])
+        f.write(json.dumps(bins));
