57a50e92c76f952a7e1777f01e15702f413ad174
Ryan Casey <ryepdx@gmail.com>
2015-09-25 17:06:27 -0400

    Move command definitions over to the dapple core plugin definitions.

--- dapple/__main__.py
+++ dapple/__main__.py
@@ -7,38 +7,11 @@ import subprocess
 import click
 from . import cli
 
-def load_dapp(buildpath="dapplefile"):
-    with open(buildpath) as buildfile:
-        buildobj = yaml.load(buildfile)
-        dapp = dapple.Dapp(buildobj)
-        return dapp
-
-@cli.command()
-@click.option('-r', '--regex', default="")
-def test(regex):
-    dapp = load_dapp()
-    dapp.build()
-    dapp.test(regex)
-
-@cli.command()
-@click.option('--stagedir', default="dapple/staging")
-def stage(stagedir):
-    dapp = load_dapp()
-    dapp.build()
-    dapp.write_binaries(stagedir)
-
-@cli.command()
-@click.argument("env")
-def chain(env):
-    if env == "testnet":
-        subprocess.call("geth --datadir devdatadir --networkid 42 --unlock 0 --password devdatadir/pass.txt --mine console", shell=True)
-    else:
-        click.echo("Unknown chain environment")
-
 if __name__ == "__main__":
     try:
         with open('.dapple/plugins', 'r') as f:
             plugins = f.readlines()
+
     except IOError:
         plugins = ['dapple_core']
         os.makedirs('.dapple')
--- dapple_core/__init__.py
+++ dapple_core/__init__.py
@@ -1,6 +1,28 @@
 from dapple import cli, click
 
 @cli.command()
-@click.option('-s', '--string', default="")
 def new(string=""):
-    print "Making new"
+    print "TBD"
+
+@cli.command()
+@click.option('-r', '--regex', default="")
+def test(regex):
+    dapp = load_dapp()
+    dapp.build()
+    dapp.test(regex)
+
+@cli.command()
+@click.option('--stagedir', default="dapple/staging")
+def stage(stagedir):
+    dapp = load_dapp()
+    dapp.build()
+    dapp.write_binaries(stagedir)
+
+@cli.command()
+@click.argument("env")
+def chain(env):
+    if env == "testnet":
+        subprocess.call("geth --datadir devdatadir --networkid 42 --unlock 0 --password devdatadir/pass.txt --mine console", shell=True)
+    else:
+        click.echo("Unknown chain environment")
+
