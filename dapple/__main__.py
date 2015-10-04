02394dc6da9ace9e2abc652a54ebc26004640299
Ryan Casey <ryepdx@gmail.com>
2015-10-04 20:31:05 +0000

    Friendlier error message when trying to run a command in a non-Dapple directory.

--- dapple/__main__.py
+++ dapple/__main__.py
@@ -1,34 +1,16 @@
-from __future__ import print_function
-from pkg_resources import resource_listdir, resource_string
-
-import os
 import importlib
-import dapple
-import yaml
-import sys
-import subprocess
-import click
-from .cli import cli
-
-def init(string=""):
-    os.makedirs('.dapple')
-
-    for fname in resource_listdir(__name__, 'defaults'):
-        with open('.dapple/' + fname, 'w') as f:
-            f.write(resource_string(__name__, 'defaults/' + fname))
-
-    print("Init'ed Dapple package. You might want to edit .dapple/dappfile now.")
+from .cli import cli, InitialCLI
 
 if "__main__" in __name__:
     try:
         with open('.dapple/plugins', 'r') as f:
             plugins = f.readlines()
 
+        for plugin in plugins:
+            importlib.import_module(plugin)
+
     except:
         plugins = []
-        cli.command()(init)
-
-    for plugin in plugins:
-        importlib.import_module(plugin)
+        cli = InitialCLI()
 
     cli()
--- dapple/cli.py
+++ dapple/cli.py
@@ -1,4 +1,34 @@
+from __future__ import print_function
+from pkg_resources import resource_listdir, resource_string
+
 import click
+import os
+import sys
+
+
+@click.command()
+def init():
+    os.makedirs('.dapple')
+
+    for fname in resource_listdir(__name__, 'defaults'):
+        with open('.dapple/' + fname, 'w') as f:
+            f.write(resource_string(__name__, 'defaults/' + fname))
+
+    print("Init'ed Dapple package. You might want to edit .dapple/dappfile now.")
+
+
+class InitialCLI(click.MultiCommand):
+    def list_commands(self, ctx):
+        return 'init'
+
+    def get_command(self, ctx, name):
+        if name == "init":
+            return init
+
+        print('This does not appear to be a Dapple package.'
+              ' Run `dapple init` first.', file=sys.stderr)
+        sys.exit(1)
+
 
 @click.group()
 def cli():
