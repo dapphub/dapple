b274375df55692aaad4f50ef10a1eb1fb192b0d3
Nikolai Mushegian <nikolai.mushegian@gmail.com>
2015-11-22 13:36:31 -0500

    existsSync

--- rewrite/lib/file.js
+++ rewrite/lib/file.js
@@ -9,5 +9,16 @@ fs.readJsonSync = function(path) {
 fs.writeJsonSync = function(path, data) {
     return fs.writeFileSync(path, JSON.stringify(data));
 }
+fs.existsSync = function(path) {
+    try {
+        var stats = fs.lstatSync(path);
+        return true;
+    } catch (e) {
+        if (e.errno == -2) {
+            return false;
+        }
+        throw e;
+    }
+}
 fs.tmpdir = os.tmpdir;
 module.exports = fs;
