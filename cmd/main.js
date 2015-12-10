8f8764fc12a1caaaee68a9b4be4c864b5d3e71b6
Ryan Casey <ryan@nexusdev.us>
2015-12-10 08:04:59 +0000

    Move all the stream piping into a Pipeline closure.

--- cmd/main.js
+++ cmd/main.js
@@ -6,11 +6,8 @@
 
 var docopt = require('docopt');
 var fs = require('fs');
-var ignore = require('gulp-ignore');
 var path = require("path");
-var streams = require('../lib/streams');
-var through = require('through2');
-var vinyl = require("vinyl-fs");
+var pipelines = require("../lib/pipelines");
 var Workspace = require("../lib/workspace");
 
 var doc = fs.readFileSync(__dirname+"/docopt.txt").toString();
@@ -20,41 +17,13 @@ var cli = docopt.docopt(doc);
 // as if it were a package and commence with building.
 if( cli.build ) {
     var workspace = new Workspace();
-    var sourceRoot = workspace.package_root;
 
-    if (workspace.dappfile.layout.sol_sources) {
-        sourceRoot = path.join(sourceRoot, workspace.dappfile.layout.sol_sources);
-    }
-
-    // Grab all the files relevant to the package.
-    streams.package_stream(sourceRoot)
-
-        // Inject a couple built-in contracts.
-        .pipe(streams.inject_virtual_contracts())
-
-        // **TODO**: Either implement a hierarchical ignore
-        // filter, or determine that files in the "ignore"
-        // pattern get filtered out during publishing.
-        // Using gulp-ignore directly for now.
-        .pipe(ignore.exclude(workspace.dappfile.ignore))
-
-        // **TODO**: Preprocessors.
-        //.pipe(streams.preprocessor())
-        
-        // **TODO**: File linking.
-        //.pipe(streams.link_files())
-
-        // **TODO**: Contract linking.
-        //.pipe(streams.link_contracts())
-        
-        // **TODO**: Add any missing steps.
-        //.pipe(...)
-
-        // Build!
-        .pipe(streams.build())
+    // Run our build pipeline.
+    pipelines.BuildPipeline(workspace.getSourceDir(),
+                            workspace.getIgnoreGlobs())
 
         // Write output to filesystem.
-        .pipe(vinyl.dest(workspace.getBuildDir()));
+        .pipe(workspace.getBuildDest());
 
 
 // If they set the `--init` flag, we just set up the current directory as a
@@ -63,4 +32,5 @@ if( cli.build ) {
 } else if (cli.init) {
     console.log(process.cwd());
     Workspace.initialize(process.cwd());
+
 }
