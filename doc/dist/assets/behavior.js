cd4beef436b613da2a5589f4ff91d5f6dc211c62
Ryan Casey <ryan@nexusdev.us>
2015-12-11 23:08:21 +0000

    Update docs.

--- doc/dist/assets/behavior.js
+++ doc/dist/assets/behavior.js
@@ -181,8 +181,7 @@ f.event={add:function(a,c,d,e,g){var h,i,j,k,l,m,n,o,p,q,r,s;if(!(a.nodeType===3
             "pageTitle": "lib/streams",
             "title": "streams"
           },
-          "depth": 2,
-          "outline": []
+          "depth": 2
         }, {
           "type": "file",
           "data": {
@@ -201,7 +200,8 @@ f.event={add:function(a,c,d,e,g){var h,i,j,k,l,m,n,o,p,q,r,s;if(!(a.nodeType===3
             "pageTitle": "lib/vmtest",
             "title": "vmtest"
           },
-          "depth": 2
+          "depth": 2,
+          "outline": []
         }, {
           "type": "file",
           "data": {
