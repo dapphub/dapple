47a35736dffcc81b7ac0f5d8e1f3c0ebeebac1b9
Nikolai Mushegian <nikolai.mushegian@gmail.com>
2015-12-11 15:46:41 -0500

    vmtest

--- test/vmtest_test.sol
+++ test/vmtest_test.sol
@@ -2,8 +2,18 @@ var vmtest = require("../lib/vmtest");
 var t = require("./testenv");
 
 describe('VM tests', function() {
-    it.skip("instantiates base Test class", function(done) {
+    it("instantiates base Test class", function(done) {
         var classes = t.golden_solc_output();
-        var result = vmtest.run(classes["Test"]);
+        var result = vmtest.run(classes["Test"], function(err, results) {
+            if(err) throw err;
+            done();
+        });
+    });
+    it("runs ExampleTest tests", function(done) {
+        var classes = t.golden_solc_output();
+        var result = vmtest.run(classes["ExampleTest"], function(err, results) {
+            if(err) throw err;
+            done();
+        });
     });
 });
