ef5dfc54296d9958a715026b1a1be389b879fca8
Ryan Casey <ryan@nexusdev.us>
2015-12-22 04:24:28 +0000

    Switch to using Lodash.

--- doc/dist/assets/behavior.js
+++ doc/dist/assets/behavior.js
@@ -86,7 +86,8 @@ f.event={add:function(a,c,d,e,g){var h,i,j,k,l,m,n,o,p,q,r,s;if(!(a.nodeType===3
             "pageTitle": "lib/build",
             "title": "build"
           },
-          "depth": 2
+          "depth": 2,
+          "outline": []
         }, {
           "type": "file",
           "data": {
@@ -219,8 +220,7 @@ f.event={add:function(a,c,d,e,g){var h,i,j,k,l,m,n,o,p,q,r,s;if(!(a.nodeType===3
             "pageTitle": "lib/streams",
             "title": "streams"
           },
-          "depth": 2,
-          "outline": []
+          "depth": 2
         }, {
           "type": "file",
           "data": {
