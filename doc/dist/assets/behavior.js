c4cc5177ef95b4f9885b5f7d7347b233c5033e95
Ryan Casey <ryan@nexusdev.us>
2016-01-06 23:03:12 +0000

    Update docs.

--- doc/dist/assets/behavior.js
+++ doc/dist/assets/behavior.js
@@ -143,8 +143,7 @@ f.event={add:function(a,c,d,e,g){var h,i,j,k,l,m,n,o,p,q,r,s;if(!(a.nodeType===3
             "pageTitle": "lib/dapplerc",
             "title": "dapplerc"
           },
-          "depth": 2,
-          "outline": []
+          "depth": 2
         }, {
           "type": "file",
           "data": {
@@ -334,7 +333,8 @@ f.event={add:function(a,c,d,e,g){var h,i,j,k,l,m,n,o,p,q,r,s;if(!(a.nodeType===3
             "pageTitle": "lib/vmtest",
             "title": "vmtest"
           },
-          "depth": 2
+          "depth": 2,
+          "outline": []
         }, {
           "type": "file",
           "data": {
