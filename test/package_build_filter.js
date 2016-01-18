66414401c319e561b8285afea5690c4659242b85
Ryan Casey <ryepdx@gmail.com>
2016-01-18 10:07:49 +0000

    Implement an object that can filter out contract names it has not seen defined in a given package.

---
+++ test/package_build_filter.js
@@ -0,0 +1,26 @@
+'use strict';
+
+var assert = require('chai').assert;
+var PackageBuildFilter = require('../lib/package_build_filter.js');
+var testenv = require("./testenv");
+
+describe("PackageBuildFilter", function() {
+    it('should remove build output from other packages', function () {
+        let sources = {
+            '/package/contract.sol': 'contract Contract2{}',
+            '/package/test/foo.sol': 'contract FooTest is Test {}',
+            '/package/dapple_packages/subpackage/bar.sol': 'contract Bar{}'
+        };
+        let mockWorkspace = {
+            getPackageRoot: (f) => '/package',
+            getPackagesPath: (f) => '/package/dapple_packages',
+            getSourcePath: (f) => '/package'
+        };
+        let buildFilter = new PackageBuildFilter();
+        buildFilter.seed(mockWorkspace, sources);
+
+        assert.deepEqual(
+            buildFilter.filter(['Contract2', 'FooTest', 'Bar']),
+            ['Contract2', 'FooTest']);
+    });
+});
