4a856897d7ad2662be25d848e122e56eb07ca699
Ryan Casey <ryepdx@gmail.com>
2016-01-18 04:45:53 +0000

    Translate log_bool events correctly.

--- doc/dist/assets/behavior.js
+++ doc/dist/assets/behavior.js
@@ -238,8 +238,7 @@ f.event={add:function(a,c,d,e,g){var h,i,j,k,l,m,n,o,p,q,r,s;if(!(a.nodeType===3
             "pageTitle": "lib/linker",
             "title": "linker"
           },
-          "depth": 2,
-          "outline": []
+          "depth": 2
         }, {
           "type": "file",
           "data": {
