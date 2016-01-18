43498170db823c04ec4e06f463f58edfc4d82dc3
Denis Erfurt <denis.erfurt@gmail.com>
2016-01-18 21:01:08 +0100

    adding dappfile spec and tests

--- test/specs.js
+++ test/specs.js
@@ -7,6 +7,7 @@ var schema = require('../specs/schema.json');
 
 var definitions = require('../specs/definitions.json');
 var dapplercSchema = require('../specs/dapplerc.json');
+var dappfileSchema = require('../specs/dappfile.json');
 
 describe('specs', function() {
   
@@ -26,6 +27,20 @@ describe('specs', function() {
     tv4.addSchema( 'http://json-schema.org/draft-04/schema', schema );
     
     var valid = tv4.validate( dapplercSchema, hyper_schema );
+    
+    assert( valid, 'dapplerc.json is not a valid schema' );
+    assert( tv4.missing.length == 0, 'some references are missing: '+JSON.stringify(tv4.missing) );
+    
+    done();
+  });
+  
+  it("dappfile should be a valid json schema ", function(done){
+    
+    tv4.addSchema( 'definitions', definitions );
+    tv4.addSchema( 'http://json-schema.org/draft-04/hyper-schema', hyper_schema );
+    tv4.addSchema( 'http://json-schema.org/draft-04/schema', schema );
+    
+    var valid = tv4.validate( dappfileSchema, hyper_schema );
     
     assert( valid, 'dapplerc.json is not a valid schema' );
     assert( tv4.missing.length == 0, 'some references are missing: '+JSON.stringify(tv4.missing) );
