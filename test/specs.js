d60be3438cd306526c7b586a19f918cc6d46b070
Ryan Casey <ryepdx@gmail.com>
2016-01-28 00:32:16 +0000

    Make tests semistandard conformant.

--- test/specs.js
+++ test/specs.js
@@ -1,3 +1,4 @@
+/* global it, describe */
 'use strict';
 var assert = require('chai').assert;
 var tv4 = require('tv4');
@@ -9,43 +10,40 @@ var definitions = require('../specs/definitions.json');
 var dapplercSchema = require('../specs/dapplerc.json');
 var dappfileSchema = require('../specs/dappfile.json');
 
-describe('specs', function() {
-  
-  it("definitions.json should be a valid json schema", function(done){
-    
+describe('specs', function () {
+  it('definitions.json should be a valid json schema', function (done) {
     var valid = tv4.validate(definitions, hyper_schema);
-    
+
     assert.isTrue(valid);
-    
+
     done();
   });
-  
-  it("dapplerc should be a valid json schema", function(done){
-    
-    tv4.addSchema( 'definitions', definitions );
-    tv4.addSchema( 'http://json-schema.org/draft-04/hyper-schema', hyper_schema );
-    tv4.addSchema( 'http://json-schema.org/draft-04/schema', schema );
-    
-    var valid = tv4.validate( dapplercSchema, hyper_schema );
-    
-    assert( valid, 'dapplerc.json is not a valid schema' );
-    assert( tv4.missing.length == 0, 'some references are missing: '+JSON.stringify(tv4.missing) );
-    
+
+  it('dapplerc should be a valid json schema', function (done) {
+    tv4.addSchema('definitions', definitions);
+    tv4.addSchema('http://json-schema.org/draft-04/hyper-schema', hyper_schema);
+    tv4.addSchema('http://json-schema.org/draft-04/schema', schema);
+
+    var valid = tv4.validate(dapplercSchema, hyper_schema);
+
+    assert(valid, 'dapplerc.json is not a valid schema');
+    assert(tv4.missing.length === 0,
+      'some references are missing: ' + JSON.stringify(tv4.missing));
+
     done();
   });
-  
-  it("dappfile should be a valid json schema ", function(done){
-    
-    tv4.addSchema( 'definitions', definitions );
-    tv4.addSchema( 'http://json-schema.org/draft-04/hyper-schema', hyper_schema );
-    tv4.addSchema( 'http://json-schema.org/draft-04/schema', schema );
-    
-    var valid = tv4.validate( dappfileSchema, hyper_schema );
-    
-    assert( valid, 'dapplerc.json is not a valid schema' );
-    assert( tv4.missing.length == 0, 'some references are missing: '+JSON.stringify(tv4.missing) );
-    
+
+  it('dappfile should be a valid json schema ', function (done) {
+    tv4.addSchema('definitions', definitions);
+    tv4.addSchema('http://json-schema.org/draft-04/hyper-schema', hyper_schema);
+    tv4.addSchema('http://json-schema.org/draft-04/schema', schema);
+
+    var valid = tv4.validate(dappfileSchema, hyper_schema);
+
+    assert(valid, 'dapplerc.json is not a valid schema');
+    assert(tv4.missing.length === 0,
+      'some references are missing: ' + JSON.stringify(tv4.missing));
+
     done();
   });
-  
 });
--- test/workspace_test.js
+++ test/workspace_test.js
@@ -1,16 +1,13 @@
+/* global it, describe */
 'use strict';
 
 var _ = require('lodash');
-var DappleRC = require('../lib/dapplerc.js');
-var expect = require("chai").expect;
-var assert = require("chai").assert;
-var fs = require("../lib/file");
-var tmp = require("tmp");
-var Workspace = require("../lib/workspace");
-var constants = require("../lib/constants");
-var testenv = require("./testenv");
-var path = require("path");
-var dircompare = require("dir-compare");
+var assert = require('chai').assert;
+var fs = require('../lib/file');
+var Workspace = require('../lib/workspace');
+var testenv = require('./testenv');
+var path = require('path');
+var dircompare = require('dir-compare');
 var tv4 = require('tv4');
 
 var definitions = require('../specs/definitions.json');
@@ -18,137 +15,134 @@ var dappfileSchema = require('../specs/dappfile.json');
 
 tv4.addSchema('definitions', definitions);
 
+describe('class Workspace', function () {
+  it('.initialize(emptydir) matches golden version', function () {
+    var dir = fs.tmpdir();
+    Workspace.initialize(dir);
+    // fs.copySync(dir, testenv.golden.INIT_EMPTY_DIR); //  Create a new golden record
+    var diff = dircompare.compareSync(dir, testenv.golden.INIT_EMPTY_DIR);
+    assert(diff.same);
+  });
 
-describe("class Workspace", function() {
-    it(".initialize(emptydir) matches golden version", function() {
-        var dir = fs.tmpdir();
-        Workspace.initialize(dir)
-        //fs.copySync(dir, testenv.golden.INIT_EMPTY_DIR); //  Create a new golden record
-        var diff = dircompare.compareSync(dir, testenv.golden.INIT_EMPTY_DIR);
-        assert( diff.same );
+  it('initializes successfully in golden package', function (done) {
+    testenv.get_source_files(
+      testenv.golden_package_dir, function (files) {
+        Workspace(_.values(files));
+        done();
+      });
+  });
+
+  it('can initialize around a single dappfile, as opposed to having to' +
+      ' take in all the dappfiles at once', function () {
+    var workspace = Workspace.atPackageRoot(testenv.golden_package_dir);
+    var expectedDappfile = fs.readYamlSync(path.join(
+      testenv.golden_package_dir, 'dappfile'));
+    assert.deepEqual(workspace.dappfile, expectedDappfile);
+
+    assert(tv4.validate(expectedDappfile, dappfileSchema),
+      'dappfile is not valid by schema');
+  });
+
+  describe('findPackageRoot', function () {
+    it('may take an argument instead of assuming the' +
+        ' search should start in the current working directory', function () {
+      let subdir = path.join(
+        testenv.golden_package_dir, 'subdirectory');
+      assert.equal(Workspace.findPackageRoot(subdir),
+        testenv.golden_package_dir);
     });
 
-    it("initializes successfully in golden package", function(done) {
-        testenv.get_source_files(
-            testenv.golden_package_dir, function (files) {
-                var workspace = new Workspace(_.values(files));
-                done();
-            });
+    it('recognizes the lowest parent directory with a dappfile' +
+        ' as the package root', function () {
+      let pkgdir = path.join(
+        testenv.golden_package_dir, 'dapple_packages', 'pkg');
+      let subdir = path.join(pkgdir, 'src', 'sol');
+      assert.equal(Workspace.findPackageRoot(subdir), pkgdir);
     });
 
-    it("can initialize around a single dappfile, as opposed to having to" +
-        " take in all the dappfiles at once", function() {
-            var workspace = Workspace.atPackageRoot(testenv.golden_package_dir);
-            var expectedDappfile = fs.readYamlSync(path.join(
-                    testenv.golden_package_dir, 'dappfile'));
-            assert.deepEqual(workspace.dappfile, expectedDappfile);
-            
-            assert( tv4.validate(expectedDappfile, dappfileSchema), "dappfile is not valid by schema" );
+    it('returns undefined if it hits root', function () {
+      var dir = fs.tmpdir();
+      assert.equal(undefined, Workspace.findPackageRoot(dir));
     });
 
-    describe("findPackageRoot", function() {
-        it("may take an argument instead of assuming the" +
-           " search should start in the current working directory",
-            function () {
-                let subdir = path.join(
-                    testenv.golden_package_dir, "subdirectory");
-                assert.equal(Workspace.findPackageRoot(subdir),
-                             testenv.golden_package_dir);
-           });
+    it.skip('returns undefined if it hits .dapplerc', function () {
+      var dir = 'TODO';
+      var workspace = new Workspace(testenv.golden_package_dir);
+      assert.equal(undefined, workspace.findPackageRoot(dir));
+    });
+  });
 
-        it("recognizes the lowest parent directory with a dappfile" +
-           " as the package root",
-            function() {
-                let pkgdir = path.join(
-                    testenv.golden_package_dir, "dapple_packages", "pkg");
-                let subdir = path.join(pkgdir, "src", "sol");
-                assert.equal(Workspace.findPackageRoot(subdir), pkgdir);
-        });
-
-        it("returns undefined if it hits root", function() {
-            var dir = fs.tmpdir();
-            assert.equal(undefined, Workspace.findPackageRoot(dir));
-        });
-
-        it.skip("returns undefined if it hits .dapplerc", function() {
-            var dir = "TODO";
-            var workspace = new Workspace(testenv.golden_package_dir);
-            assert.equal( undefined, workspace.findPackageRoot(dir) );
-        });
+  describe('findBuildPath', function () {
+    it('may take an argument instead of assuming the' +
+    ' search should start in the current working directory', function () {
+      assert.equal(
+        Workspace.findBuildPath(testenv.golden_package_dir),
+        path.join(testenv.golden_package_dir, 'build'));
     });
 
-    describe("findBuildPath", function() {
-        it("may take an argument instead of assuming the" +
-           " search should start in the current working directory",
-           function () {
-               assert.equal(
-                   Workspace.findBuildPath(testenv.golden_package_dir),
-                   path.join(testenv.golden_package_dir, 'build'));
-           });
-
-        it("returns undefined if it hits root", function() {
-            var dir = fs.tmpdir();
-            assert.equal(undefined, Workspace.findBuildPath(dir));
-        });
+    it('returns undefined if it hits root', function () {
+      var dir = fs.tmpdir();
+      assert.equal(undefined, Workspace.findBuildPath(dir));
     });
+  });
 
-    it('knows how to load .dapplerc', function() {
-        var fixtureRC = path.join(__dirname, '_fixtures', 'dapplerc');
-        var rc = Workspace.getDappleRC({paths: [fixtureRC]});
-        var expectedRC = fs.readYamlSync(fixtureRC + '.expanded');
-        assert.deepEqual(rc.data, expectedRC, "did not load " + fixtureRC);
-    });
+  it('knows how to load .dapplerc', function () {
+    var fixtureRC = path.join(__dirname, '_fixtures', 'dapplerc');
+    var rc = Workspace.getDappleRC({paths: [fixtureRC]});
+    var expectedRC = fs.readYamlSync(fixtureRC + '.expanded');
+    assert.deepEqual(rc.data, expectedRC, 'did not load ' + fixtureRC);
+  });
 
-    it('knows how to make .dapplerc', function() {
-        var fixtureRC = path.join(__dirname, '_fixtures', 'dapplerc');
-        var expectedRC = fs.readYamlSync(fixtureRC + '.expanded');
-        var newRC = path.join(__dirname, '_fixtures', 'dapplerc_copy');
-        Workspace.writeDappleRC(newRC, expectedRC);
-        var rc = Workspace.getDappleRC({paths: [newRC]});
-        fs.removeSync(newRC); // Cleanup
+  it('knows how to make .dapplerc', function () {
+    var fixtureRC = path.join(__dirname, '_fixtures', 'dapplerc');
+    var expectedRC = fs.readYamlSync(fixtureRC + '.expanded');
+    var newRC = path.join(__dirname, '_fixtures', 'dapplerc_copy');
+    Workspace.writeDappleRC(newRC, expectedRC);
+    var rc = Workspace.getDappleRC({paths: [newRC]});
+    fs.removeSync(newRC); // Cleanup
 
-        assert.deepEqual(rc.data, expectedRC, "did not create dapplerc");
-    });
+    assert.deepEqual(rc.data, expectedRC, 'did not create dapplerc');
+  });
 
-    it('does not throw an exception if the dappfile is empty', function() {
-        var workspace = new Workspace(testenv.empty_package_dir);
-    });
+  it('does not throw an exception if the dappfile is empty', function () {
+    Workspace.create(testenv.empty_package_dir);
+  });
 
-    it('run its getters on an empty dappfile without throwing', function() {
-        var workspace = new Workspace(testenv.empty_package_dir);
-        var getters = [
-            "getBuildDir",
-            "getDappfilePath",
-            "getDependencies",
-            "getEnvironment",
-            "getEnvironments",
-            "getIgnoreGlobs",
-            "getPackagesDir",
-            "getPackagesPath",
-            "getPreprocessorVars",
-            "getSourceDir",
-            "getSourcePath",
-        ];
+  it('run its getters on an empty dappfile without throwing', function () {
+    var workspace = new Workspace(testenv.empty_package_dir);
+    var getters = [
+      'getBuildDir',
+      'getDappfilePath',
+      'getDependencies',
+      'getEnvironment',
+      'getEnvironments',
+      'getIgnoreGlobs',
+      'getPackagesDir',
+      'getPackagesPath',
+      'getPreprocessorVars',
+      'getSourceDir',
+      'getSourcePath'
+    ];
 
-        for (let getter of getters) {
-            assert.doesNotThrow(workspace[getter].bind(workspace),
-                                Error, getter + ' threw!');
-        }
-    });
+    for (let getter of getters) {
+      assert.doesNotThrow(workspace[getter].bind(workspace),
+        Error, getter + ' threw!');
+    }
+  });
 
-    it('allows setting subordinate dappfiles, even empty ones', function() {
-        var workspace = new Workspace(testenv.golden_package_dir);
-        workspace.addSubDappfile(process.cwd(), {});
-    });
+  it('allows setting subordinate dappfiles, even empty ones', function () {
+    var workspace = new Workspace(testenv.golden_package_dir);
+    workspace.addSubDappfile(process.cwd(), {});
+  });
 
-    it('allows setting sub-dappfiles with ignore patterns', function() {
-        var cwd = process.cwd();
-        var workspace = new Workspace(testenv.golden_package_dir);
-        var prevIgnores = workspace.getIgnoreGlobs();
-        workspace.addSubDappfile(cwd, {ignore: ['foo/bar.sol']});
-        assert.deepEqual(
-            _.difference(workspace.getIgnoreGlobs(), prevIgnores), [
-                path.join(cwd, 'foo/bar.sol')
-            ]);
-    });
+  it('allows setting sub-dappfiles with ignore patterns', function () {
+    var cwd = process.cwd();
+    var workspace = new Workspace(testenv.golden_package_dir);
+    var prevIgnores = workspace.getIgnoreGlobs();
+    workspace.addSubDappfile(cwd, {ignore: ['foo/bar.sol']});
+    assert.deepEqual(
+      _.difference(workspace.getIgnoreGlobs(), prevIgnores), [
+        path.join(cwd, 'foo/bar.sol')
+      ]);
+  });
 });
