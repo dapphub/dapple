d1ecbb6f64ba58e6c141c9c8896ac8a3be524c7c
Denis Erfurt <denis.erfurt@gmail.com>
2016-01-28 12:47:40 +0100

    [+] add test for expectEvent test predicate

--- test/_fixtures/mocktest.sol
+++ test/_fixtures/mocktest.sol
@@ -2,6 +2,13 @@ contract Test {
     bool public failed;
 
     event logs(bytes val);
+    
+    event eventListener(bytes32 name);
+    
+    modifier expectEvent(bytes32 name) {
+      eventListener( name );
+      _
+    }
 
     function fail() {
         failed = true;
@@ -35,6 +42,51 @@ contract Throw is Test {
 
 contract NotThrow is Test {
     function testThrow() {
+      assertTrue(true);
+    }
+}
+
+contract Event is Test {
+  
+    event foo(bytes val);
+
+    function testEvent() expectEvent("foo") {
+      foo("uiuiui");
+      assertTrue(true);
+    }
+}
+
+
+contract TwoEvent is Test {
+  
+    event foo(bytes val);
+    event bar(bytes val);
+
+    function testEvent() expectEvent("foo") expectEvent("bar") {
+      foo("uiuiui");
+      bar("uiuiui");
+      assertTrue(true);
+    }
+}
+
+
+contract NoEvent is Test {
+  
+    event foo(bytes val);
+
+    function testWhatever() expectEvent("foo") expectEvent("bar") {
+      assertTrue(true);
+    }
+}
+
+
+contract NoSecondEvent is Test {
+  
+    event foo(bytes val);
+    event bar(bytes val);
+
+    function testWhatever() expectEvent("foo") expectEvent("bar") {
+      foo("uiuiui");
       assertTrue(true);
     }
 }
--- test/vmtest_test.js
+++ test/vmtest_test.js
@@ -60,7 +60,6 @@ describe("VMTest", function() {
         assert.notOk(result.failed, "test failed, should have passed");
         done();
       });
-      
     });
     
     it("fails if an expected exception is not thrown", function( done ) {
@@ -71,6 +70,50 @@ describe("VMTest", function() {
         assert.ok(result.failed, "test passed, should have failed");
         done();
       });
-      
     });
+    
+    it("catches expected event", function( done ) {
+      var contract = new Contract(mock.contracts.Event);
+      var vmtest = new VMTest(Web3Factory.EVM(), contract);
+      vmtest.runTest(0, function(err, result) {
+        assert.notOk(err);
+        assert.notOk(result.failed, "test failed, should have passed");
+        assert.equal(1, result.logs.length);
+        done();
+      });
+    });
+    
+    it("catches multiple expected events", function( done ) {
+      var contract = new Contract(mock.contracts.TwoEvent);
+      var vmtest = new VMTest(Web3Factory.EVM(), contract);
+      vmtest.runTest(0, function(err, result) {
+        assert.notOk(err);
+        assert.notOk(result.failed, "test failed, should have passed");
+        assert.equal(2, result.logs.length);
+        done();
+      });
+    });
+    
+    it("fails if expected event is not thrown", function( done ) {
+      var contract = new Contract(mock.contracts.NoEvent);
+      var vmtest = new VMTest(Web3Factory.EVM(), contract);
+      vmtest.runTest(0, function(err, result) {
+        assert.notOk(err);
+        assert.ok(result.failed, "test passed, should have failed");
+        assert.equal(0, result.logs.length);
+        done();
+      });
+    });
+    
+    it("fails if expected events are not thrown", function( done ) {
+      var contract = new Contract(mock.contracts.NoSecondEvent);
+      var vmtest = new VMTest(Web3Factory.EVM(), contract);
+      vmtest.runTest(0, function(err, result) {
+        assert.notOk(err);
+        assert.ok(result.failed, "test passed, should have failed");
+        assert.equal(1, result.logs.length);
+        done();
+      });
+    });
+    
 });
