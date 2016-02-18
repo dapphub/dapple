f851a112ca97ea373d5801f58b1e3d33a9951b16
Ryan Casey <ryepdx@gmail.com>
2016-02-18 19:49:05 +0000

    Require git commit hashes and don't fallback to Github.

--- test/dependency_test.js
+++ test/dependency_test.js
@@ -6,65 +6,39 @@ var Dependency = require('../lib/dependency');
 
 describe('Dependency', function () {
   describe('construction from strings resolving to git paths', function () {
-    // We'll be changing this to prefer Dapphub paths once that's launched.
-    it('translates NexusDev/dappsys to a Github URL', function () {
-      var dep = Dependency.fromDependencyString('NexusDev/dappsys');
-      assert(dep.hasGitPath(), 'should have gotten a git path');
-      assert.equal(dep.getName(), '');
-      assert.equal(dep.getPath(), 'https://github.com/NexusDev/dappsys.git');
-      assert.equal(dep.getVersion(), '');
-    });
-
-    it('translates NexusDev/dappsys@9fe4f51 to a Github URL', function () {
-      var dep = Dependency.fromDependencyString('NexusDev/dappsys@9fe4f51');
-      assert(dep.hasGitPath(), 'should have gotten a git path');
-      assert.equal(dep.getName(), '');
-      assert.equal(dep.getPath(), 'https://github.com/NexusDev/dappsys.git');
-      assert.equal(dep.getVersion(), '@9fe4f51');
-    });
-
-    it('translates NexusDev/dappsys#audit@9fe4f51 to a Github URL', function () {
-      var dep = Dependency.fromDependencyString('NexusDev/dappsys#audit@9fe4f51');
-      assert(dep.hasGitPath(), 'should have gotten a git path');
-      assert.equal(dep.getName(), '');
-      assert.equal(dep.getPath(), 'https://github.com/NexusDev/dappsys.git');
-      assert.equal(dep.getVersion(), '#audit@9fe4f51');
-    });
-
-    it('allows overriding the name property', function () {
-      var dep = Dependency.fromDependencyString('NexusDev/dappsys', 'dappsys2');
-      assert(dep.hasGitPath(), 'should have gotten a git path');
-      assert.equal(dep.getName(), 'dappsys2');
-      assert.equal(dep.getPath(), 'https://github.com/NexusDev/dappsys.git');
-      assert.equal(dep.getVersion(), '');
-    });
-
-    it('handles non-short non-Github git addresses as well', function () {
+    it('handles git addresses', function () {
       var dep = Dependency.fromDependencyString(
-        'https://bitbucket.org/nexusdev/dappsys.git#audit@9fe4f51');
+        'https://github.com/nexusdev/dappsys.git@9fe4f51');
       assert(dep.hasGitPath(), 'should have gotten a git path');
       assert.equal(dep.getName(), '');
-      assert.equal(dep.getPath(), 'https://bitbucket.org/nexusdev/dappsys.git');
-      assert.equal(dep.getVersion(), '#audit@9fe4f51');
+      assert.equal(dep.getPath(), 'https://github.com/nexusdev/dappsys.git');
+      assert.equal(dep.getVersion(), '9fe4f51');
     });
 
-    it('handles non-Github git addresses with auth as well', function () {
+    it('requires a commit hash', function () {
+      assert.throws(function () {
+        Dependency.fromDependencyString(
+          'https://github.com/nexusdev/dappsys.git');
+      });
+    });
+
+    it('handles git addresses with auth as well', function () {
       var dep = Dependency.fromDependencyString(
-        'dev@bitbucket.org/nexusdev/dappsys.git#audit@9fe4f51');
+        'dev@bitbucket.org/nexusdev/dappsys.git@9fe4f51');
       assert.equal(dep.getPath(), 'dev@bitbucket.org/nexusdev/dappsys.git');
       assert(dep.hasGitPath(), 'should have gotten a git path');
       assert.equal(dep.getName(), '');
       assert.equal(dep.getPath(), 'dev@bitbucket.org/nexusdev/dappsys.git');
-      assert.equal(dep.getVersion(), '#audit@9fe4f51');
+      assert.equal(dep.getVersion(), '9fe4f51');
     });
 
     it('matches git addresses regardless of case', function () {
       var dep = Dependency.fromDependencyString(
-        'Dev@BitBucket.org/NexusDev/Dappsys.Git#audit@9fe4f51');
+        'Dev@BitBucket.org/NexusDev/Dappsys.Git@9fe4f51');
       assert(dep.hasGitPath(), 'should have gotten a git path');
       assert.equal(dep.getName(), '');
       assert.equal(dep.getPath(), 'Dev@BitBucket.org/NexusDev/Dappsys.Git');
-      assert.equal(dep.getVersion(), '#audit@9fe4f51');
+      assert.equal(dep.getVersion(), '9fe4f51');
     });
   });
 
@@ -113,6 +87,14 @@ describe('Dependency', function () {
       var dep = Dependency.fromDependencyString('ipfs://Qmdeadbeef');
       assert(dep.hasIPFSPath(), 'should have gotten an IPFS path');
       assert.equal(dep.getName(), '');
+      assert.equal(dep.getPath(), 'ipfs://Qmdeadbeef');
+      assert.equal(dep.getVersion(), 'Qmdeadbeef');
+    });
+
+    it('allows overriding names of IPFS packages', function () {
+      var dep = Dependency.fromDependencyString('dappsys2@Qmdeadbeef');
+      assert(dep.hasIPFSPath(), 'should have gotten an IPFS path');
+      assert.equal(dep.getName(), 'dappsys2');
       assert.equal(dep.getPath(), 'ipfs://Qmdeadbeef');
       assert.equal(dep.getVersion(), 'Qmdeadbeef');
     });
