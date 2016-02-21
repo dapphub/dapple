ebd9ff2be3e22d587cebbfaf74c2b6a469eafbea
Denis Erfurt <denis.erfurt@gmail.com>
2016-02-21 20:13:25 +0100

    refactor package schema and rewrite test

--- test/package_test.js
+++ test/package_test.js
@@ -1,45 +1,41 @@
 /* global it, describe */
 'use strict';
 
-var _ = require('lodash');
 var assert = require('chai').assert;
-var fs = require('../lib/file');
-var path = require('path');
-var testenv = require('./testenv');
 var schemas = require('../lib/schemas.js');
 
-describe('package', function() {
-  
-  it("should accept valide package description", function(done){
-    
+describe('package', function () {
+  it('should accept valide package description', function (done) {
     // IPFS link: QmcVb2DkBVJqT4Pw3X9Ge3dzbgSzW7pvDBsxhVs1xGPLV3
     var pkg_definition = {
-      "name": "foobar",
-      "description": "this is a test package",
-      "version": "12.0.34",
-      "tags": ["foo", "bar", "test"],
-      "root": "QmXuaDz3Nwmiz38FzG4GpxKAF4fGML1YxZPbKa6pYQcQ9B",
-      "dependencies": {
-        "some other pkg": "this should be an ipfs link"
+      'name': 'foobar',
+      'summery': 'this is a test package',
+      'version': '12.0.34',
+      'solc': {
+        'version': 'Version: 0.2.2-37381072/.-Darwin/unknown/JIT linked to libethereum-1.1.1-8138fe14/.-Darwin/unknown/JIT',
+        'flags': '--optimize'
       },
-      "environments": {
-        "morden": {
-          "objects": {
-            "wow": {
-              "class": "much",
-              "address": "package"
+      'tags': ['foo', 'bar', 'test'],
+      'root': 'QmXuaDz3Nwmiz38FzG4GpxKAF4fGML1YxZPbKa6pYQcQ9B',
+      'contracts': {
+        'OMG': 'this is another ipfs hash'
+      },
+      'dependencies': {
+        'some other pkg': 'this should be an ipfs link'
+      },
+      'environments': {
+        'morden': {
+          'objects': {
+            'wow': {
+              'class': 'much',
+              'address': 'package'
             }
           }
         }
       }
     };
-    
-    var valide = schemas.packfile.validate(pkg_definition);
-    
+    var valide = schemas.package.validate(pkg_definition);
     assert(valide);
-    
     done();
   });
-  
-  
 });
